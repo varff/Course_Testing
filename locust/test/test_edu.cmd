7063825fcd33e310859f521304533d8d1a6208f0
varff <62174550+varff@users.noreply.github.com>
2021-10-08 22:03:42 +0300

    cmd+refactoring

---
+++ locust/test/test_edu.cmd
@@ -0,0 +1,1 @@
+locust -f locust_test_edu.py --host https://edufpmi.bsu.by/ --users 1 --spawn-rate 25
---
+++ locust/test/test_git.cmd
@@ -0,0 +1,1 @@
+locust -f locust_test_git.py --host https://github.com --users 5 --spawn-rate 25
